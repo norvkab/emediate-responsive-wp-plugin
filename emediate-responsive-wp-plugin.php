af126e359d1fc5ef39348eda595deaa667d54806
Victor Jonsson <kontakt@victorjonsson.se>
2014-02-17 13:05:10 +0100

    Update emediate-responsive-wp-plugin.php

--- emediate-responsive-wp-plugin.php
+++ emediate-responsive-wp-plugin.php
@@ -5,15 +5,13 @@ Description: Integrates the website with Emediate ad manager
 Version: 0.1
 Author: norvkab <https://github.com/norvkab/>
 */
-
-
 define('ERWP_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('ERWP_PLUGIN_VERSION', '0.1.1');
 define('ERWP_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
-include_once (ERWP_PLUGIN_PATH.'classes/functions.php');
+
 require_once ERWP_PLUGIN_PATH.'/classes/class-loader.php';
 
-if(is_admin()){
+if( is_admin() ) {
     add_action('admin_menu', function() {
         $js_hook = add_options_page(
             'Emediate',
