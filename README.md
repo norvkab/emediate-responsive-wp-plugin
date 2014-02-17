a357fcfad34528638dc28016fbc54b504e082cbf
Victor Jonsson <kontakt@victorjonsson.se>
2014-02-17 12:11:20 +0100

    JS implementation wip...

--- README.md
+++ README.md
@@ -2,3 +2,67 @@ emediate-responsive-wp-plugin
 =============================
 
 Collaboration between Norran, VK and Aftonbladet.
+
+## Admin page
+
+This section of the documentation will go through how you setup the plugin using the settings page in wp-admin
+
+**TODO: write important stuff...**
+
+
+## Theme implementation
+
+This section of the documentation will go through the javascript events that's triggered by the plugin
+when the ads gets rendered.
+
+
+#### Ad server requests
+
+Each ad will make a request to the ad server containing a CU-parameter and a query-string containing information
+about the current page, this looks something like `?cre=mu;js=y;target=_blank;cu=25027;...`. You can modify the
+ad query on front-end by using the action `wp_head`, Example:
+
+```php
+<?php
+add_action('wp_head', function() { ?>
+    <script>
+        erwpSettings.adQuery += ';isHandheld=yes';
+    </script>
+<?php });
+```
+
+#### Events
+
+`erwpBreakPointChange` — Called each time the client has entered a new break point. This happens when you change
+the size of the browser window or when you change rotation of a tablet.
+
+`erwpShouldHideAd` - The plugin will check if the HTML of an ad contains <!-- no matching campaign --> to determine
+if the ad should be hidden. This event makes it possible to do more checks against the HTML content of the add. If you
+return true from this callback the add will become hidden.
+
+`erwpAdRemoved` - This event is called every time an ad becomes hidden.
+
+`erwpFiFRendered` — This event is called every time an ad is rendered
+
+
+```js
+$(window)
+    .on('erwpBreakPointChange', function() {
+        // We have entered a new break point
+    })
+    .on('erwpShouldHideAd', function(evt, fifWindow) {
+        // If an ad iframe contains this special comment we should hide the ad
+        if( fifWindow.body.innerHTML.indexOf('<!-- my-custom-no-ad-comment -->') > -1 ) {
+            return true;
+        }
+    })
+    .on('erwpAdHidden', function(evt, $fifAdElement) {
+        // apply a special class on ad containers containing an empty ad
+        $fifAdElement.parent().addClass('no-ad-here');
+    })
+    .on('erwpFifCreated', function(evt, adSrc, $fifAdElement) {
+        // The iframe is rendered, soon there will be an ad here...
+        // Lets remove the .no-ad-here class in case it was added on previous break point
+        $fifAdElement.parent().removeClass('no-ad-here');
+    })
+```
---
+++ js/ERWP.js
@@ -0,0 +1,196 @@
+var ERWP = (function($, erwpSettings) {
+
+    'use strict';
+
+    var $win = $(window),
+        ERWP = {
+
+            /**
+             * Current window size
+             * @var {Number}
+             */
+            winWidth : 0,
+
+            /**
+             * Current break point
+             * @var {Object|Boolean}
+             */
+            breakPoint : false,
+
+            /**
+             * Array with html elements containing fif-ads (jQuery).
+             * This array will be populated while the page gets rendered
+             * by calling ERWP.fif(...)
+             *
+             * @var {jQuery[]}
+             */
+            $fifAds : [],
+
+            /**
+             * Whether or not we support break points
+             * @var {Boolean}
+             */
+            hasRegisteredBreakPoints : $.isArray(erwpSettings.breakPoints) && erwpSettings.breakPoints.length,
+
+
+            /**
+             * Prints out an emediate ad using composed javascript
+             * @param {String} src
+             * @param {String|Number} cu
+             */
+            composed : function(src, cu) {
+                var clickURL = 'http://' + erwpSettings.defaultJSHost + '/eas?'+erwpSettings.cuParamName+'='+cu+';ty=ct';
+                document.write(
+                    '<script src="'+src+';'+erwpSettings.adQuery+'"></script>'+
+                    '<noscript><a target="_blank" href="'+clickURL+'">'+
+                        '<img src="'+src+';cre=img" alt="emediate" /></a></noscript>'
+                );
+            },
+
+            /**
+             * Finds ad-element at given index and inserts
+             * a fif-ad in that element
+             * @param {Number} fifIndex
+             */
+            fif : function(fifIndex) {
+                var $elem = $('#emediate-fif-'+fifIndex);
+
+                // Collect this ad element for later (when ad gets
+                // reloaded due to rotation change for example)
+                if( this.hasRegisteredBreakPoints ) {
+                    this.$fifAds.push($elem);
+                }
+
+                // render ad
+                this.renderFifAd($elem);
+            },
+
+            /**
+             * Inserts a fif-ad in given element
+             * @param {jQuery} $elem
+             */
+            renderFifAd : function($elem) {
+
+                var cu, src,
+                    height = $elem.attr('data-height') || 0,
+                    width = $elem.attr('data-width') || 0;
+
+                if( this.breakPoint ) {
+                    cu = $elem.attr('data-bp-'+this.breakPoint.min+'-'+this.breakPoint.max);
+                }
+                if( !cu ) {
+                    cu = $elem.attr('data-cu');
+                }
+
+                if( $elem.attr('data-current-cu') != cu ) {
+
+                    $elem.html(''); // clear possibly previous ad
+
+                    $elem.attr('data-current-cu', cu);
+
+                    src = 'http://'+erwpSettings.defaultJSHost+'/eas?cre=mu;js=y;target=_blank;'+erwpSettings.cuParamName+'='+cu+
+                        ';'+erwpSettings.adQuery;
+
+                    // Load fif
+                    window.EAS_load_fif('emediate-fif-'+$elem.attr('data-index'), erwpSettings.fifHtmlFile, src, width, height);
+
+                    $win.trigger('erwpFifCreated', [src, $elem]);
+                }
+            },
+
+            /**
+             * Figures out the current break point that we're in
+             * @return {Object|Boolean}
+             */
+            currentBreakPoint : function() {
+                var _self = this,
+                    current = false;
+
+                $.each(erwpSettings.breakPoints, function(i, bp) {
+                    if( _self.winWidth >= bp.min && _self.winWidth < bp.max ) {
+                        current = bp;
+                        return false;
+                    }
+                });
+
+                return current;
+            },
+
+            /**
+             * Iterate through all fif-ad elements and re-insert fif-ad
+             */
+            reloadFiFAds : function() {
+                var _self = this;
+                $.each(ERWP.$fifAds, function(i, $adElem) {
+                    _self.renderFifAd($adElem);
+                });
+            },
+
+            /**
+             * Function called by an iframe, containing a fif-ad, when the iframe
+             * document gets loaded
+             *
+             * @param {Window} iframeWin
+             */
+            fifLoaded : function(iframeWin) {
+                if( this.shouldHideFif(iframeWin) ) {
+                    var $adElem = this.getAdElementFromFifIframe(iframeWin);
+                    $adElem.html('');
+                    $win.trigger('erwpAdHidden', [$ad]);
+                }
+            },
+
+            /**
+             * @param {Window} iframeWin
+             * @return {jQuery}
+             */
+            getAdElementFromFifIframe : function(iframeWin) {
+                var cu = iframeWin.frameElement.EAS_src.split(erwpSettings.cuParamName+'=')[1].split(';')[0];
+                return $('.emediate-ad[data-current-cu="'+cu+'"]');
+            },
+
+            /**
+             * @param fifWin
+             * @returns {Boolean}
+             */
+            shouldHideFif : function (fifWin) {
+                var hasNoMatchingCampaignComment = function () {
+                        if ( !fifWin.body ) return false;
+                        return (-1 !== fifWin.body.innerHTML.indexOf('<!-- No matching campaign -->'));
+                    },
+                    containsAllScriptNodes = function () {
+                        // No comment but no other elements either, check if div-container has anything more than the iframe
+                        return fifWin.document.querySelectorAll("body script").length === fifWin.document.querySelectorAll("body *").length;
+                    };
+
+                return hasNoMatchingCampaignComment() ||
+                        containsAllScriptNodes() ||
+                        $win.trigger('erwpShouldHideAd', [fifWin]) === true;
+            }
+        };
+
+
+    if( ERWP.hasRegisteredBreakPoints ) {
+
+        // figure out current break point
+        ERWP.winWidth = $win.width();
+        ERWP.breakPoint = ERWP.currentBreakPoint();
+
+        // Capture window width and figure out current break-point
+        // when window size changes. If we have a new break-point
+        // all fif ads gets reloaded
+        $win.on('resize', function() {
+            ERWP.winWidth = $win.width();
+            var breakPoint = ERWP.currentBreakPoint();
+            if( breakPoint != ERWP.breakPoint ) {
+                // break point has changed
+                ERWP.breakPoint = breakPoint;
+                ERWP.reloadFiFAds();
+                $win.trigger('erwpBreakPointChange');
+            }
+        });
+    }
+
+    return ERWP;
+
+})(jQuery, window.erwpSettings || {});
